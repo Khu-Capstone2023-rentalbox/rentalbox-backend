2934448555a1bf76946cc6111002a2c62f791d87
CYY1007 <younguk86@gmail.com>
2023-03-28 22:32:52 +0900

    (SET UP) setup!03/28

--- config/express.js
+++ config/express.js
@@ -1,5 +1,16 @@
 import express from "express"
+import cors from "cors"
+import morgan from "morgan";
 
 const app = express();
+const logger = morgan("dev")
+
+app.use(logger)
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+app.use(cors())
+
+
 
 export default app;
