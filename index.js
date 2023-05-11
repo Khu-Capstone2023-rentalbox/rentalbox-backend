54f05e7f4b21fe4e1ca28f11c3b5edd260f77ca0
YONGWOOK CHOI <60510921+CYY1007@users.noreply.github.com>
2023-05-11 11:31:13 +0900

    Issue/24 (#25)
    * Feat userRouter, itemRouter 1차 시그니처 작성, middleware 중 jwt middleware 추가
    
    * feat : post : /users/sign-up 구현, fix : userService.js 파일에서 connection.release() 추가
    
    * feat : GET /search?keyword 구현
    
    * feat : 사진 s3에 업로드하기 구현 update : 검색 시 페이징 구현
    
    * feat : batch 작업 추가 - 연체자 찾아내기, 그리고 연체 벌금 업데이트 매일 새벽 2시에 자동으로 실행
    
    ---------
    
    Co-authored-by: viitamin <tmdals0708@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import app from "./config/express";
 import privateInfo from "./config/privateInfo";
+import schedule from "node-schedule"
+import { job } from "./src/batch/batch";
+
 
 try{
 app.listen(privateInfo.PORT, () => console.log(`server is ready! on port ${privateInfo.PORT}`))
 }catch(e){
     console.log("something is wrong")
 }
+
+schedule.scheduleJob('0 2 * * *', job)
